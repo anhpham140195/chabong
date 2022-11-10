8559fb870f42e16723877bca668bd01a64b4c5ba
Anh Pham <tuananh.pham@emma-sleep.com>
2022-11-10 21:23:04 +0700

    Update home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -355,3 +355,61 @@
 	});
 
 })(jQuery);
+
+(function () {
+	const second = 1000,
+		  minute = second * 60,
+		  hour = minute * 60,
+		  day = hour * 24;
+  
+	//I'm adding this section so I don't have to keep updating this pen every year :-)
+	//remove this if you don't need it
+	let today = new Date(),
+		dd = String(today.getDate()).padStart(2, "0"),
+		mm = String(today.getMonth() + 1).padStart(2, "0"),
+		yyyy = today.getFullYear(),
+		nextYear = yyyy,
+		dayMonth = "11/19/",
+		birthday = dayMonth + yyyy;
+	
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+	  birthday = dayMonth + nextYear;
+	}
+	//end
+	
+	const countDown = new Date(birthday).getTime(),
+		x = setInterval(function() {    
+  
+		  const now = new Date().getTime(),
+				distance = countDown - now;
+  
+		  document.getElementById("days").innerText = Math.floor(distance / (day)),
+			document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+			document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+		  //do something later when date is reached
+		  if (distance < 0) {
+			document.getElementById("headline").innerText = "It's my birthday!";
+			document.getElementById("countdown").style.display = "none";
+			document.getElementById("content").style.display = "block";
+			clearInterval(x);
+		  }
+		  //seconds
+		}, 0)
+	}());
+
+
+
+async function onSubmit(){
+	const chatId = document.getElementById('chatId').value
+	const name = document.getElementById('guestName').value
+	const message = document.getElementById('message').value
+		const text = `Tên: ${name} %0A Nội dung: ${message}`
+		const url =`https://api.telegram.org/bot5629002179:AAHdm2tAKhg1oB-IcfOKUNuKzaMzaEGUVag/sendMessage?chat_id=${chatId}&text=${text}`
+		let response = await fetch(url, {
+			method: 'GET',
+		});
+	
+}
